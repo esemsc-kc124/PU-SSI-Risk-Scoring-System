209327fe518e650dd6d0ee5565dd4641b2db8661
“esemsc-kc124” <kc124@ic.ac.uk>
2025-05-31 11:14:16 +0100

    add debug info

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -11,33 +11,17 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
-          type: 'Observation',
-          query: {
-            code: {
-              $or: [
-                'http://loinc.org|8302-2',
-                'http://loinc.org|8462-4',
-                'http://loinc.org|8480-6',
-                'http://loinc.org|2085-9',
-                'http://loinc.org|2089-1',
-                'http://loinc.org|55284-4'
-              ]
-            }
-          }
+
+        // 🔍 只获取 Condition 资源
+        var cond = smart.patient.api.fetchAll({
+          type: 'Condition',
+          pageLimit: 1  // 限制分页避免超时
         });
 
-        // 新增 FHIR 资源访问
-        var conditionPromise = smart.patient.api.fetchAll({ type: 'Condition' });
-        var procedurePromise = smart.patient.api.fetchAll({ type: 'Procedure' });
-        var encounterPromise = smart.patient.api.fetchAll({ type: 'Encounter' });
+        $.when(pt, cond).fail(onError);
 
-        $.when(pt, obv, conditionPromise, procedurePromise, encounterPromise).fail(onError);
-
-        $.when(pt, obv, conditionPromise, procedurePromise, encounterPromise).done(function(patient, obv, conditions, procedures, encounters) {
-          var byCodes = smart.byCodes(obv, 'code');
+        $.when(pt, cond).done(function(patient, cond) {
           var gender = patient.gender;
-
           var fname = '';
           var lname = '';
 
@@ -46,35 +30,13 @@
             lname = patient.name[0].family;
           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
-
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
 
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
-
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
-
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
-
-          // 控制台输出新资源数据，便于调试验证
-          console.log("✅ Conditions:", conditions);
-          console.log("✅ Procedures:", procedures);
-          console.log("✅ Encounters:", encounters);
-
+          console.log("✅ Condition fetched successfully:", cond);
           ret.resolve(p);
         });
       } else {
@@ -92,40 +54,7 @@
       lname: {value: ''},
       gender: {value: ''},
       birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
     };
-  }
-
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
-    var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
-        });
-      });
-      if (BP) {
-        observation.valueQuantity = BP.valueQuantity;
-        formattedBPObservations.push(observation);
-      }
-    });
-
-    return getQuantityValueAndUnit(formattedBPObservations[0]);
-  }
-
-  function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-      return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
-    } else {
-      return undefined;
-    }
   }
 
   window.drawVisualization = function(p) {
@@ -135,11 +64,5 @@
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
   };
-
 })(window);
